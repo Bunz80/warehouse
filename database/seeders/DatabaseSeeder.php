c5543ae8fa963123249f26162ddd2285ea5778fc
Bunz80 <sp.bunz@gmail.com>
2022-12-01 16:56:05 +0100

    database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,8 +25,8 @@ class DatabaseSeeder extends Seeder
             CustomerSeeder::class,
             // warehouse
             ProductSeeder::class,
-            OrderSeeder::class,
-            OrderDetailSeeder::class,
+            // OrderSeeder::class,
+            // OrderDetailSeeder::class,
         ]);
     }
 }
