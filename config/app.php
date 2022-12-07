6aa07676af948704da640e232c92c7a2a63b6fa8
Bunz80 <sp.bunz@gmail.com>
2022-12-07 09:15:37 +0100

    remove package heloufir/filament-workflow-manager

--- config/app.php
+++ config/app.php
@@ -181,7 +181,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        // Heloufir\FilamentWorkflowManager\FilamentWorkflowManagerServiceProvider::class,
 
         /*
          * Package Service Providers...
