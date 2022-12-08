b1a1da04a61091dd81602c767c2fb79e2aba3853
devweb <dev.webok123@gmail.com>
2022-12-08 10:28:21 -0500

    update

--- app/Filament/Resources/Warehouse/OrderResource.php
+++ app/Filament/Resources/Warehouse/OrderResource.php
@@ -234,15 +234,16 @@ class OrderResource extends Resource
                             ->content(
                                 function (Closure $get) {
                                     $company = Company::where('id', $get('company_id'));
-                                    $company_name = $company ? $company->pluck('name')[0] : '';
+                                    $company = Company::where("id", $get("company_id"));
+                                    $company_name = $company && count(company->pluck("name")) > 0 ? $company->pluck("name")[0] : "";
 
-                                    $address = Address::where('id', $get('address_id'));
-                                    $address_name = $address ? $address->pluck('name')[0] : '';
-                                    $address_street = $address ? $address->pluck('address')[0].'-'.$address->pluck('street_number')[0].', '.$address->pluck('city')[0] : '';
-                                    $address_state = $address ? $address->pluck('province')[0].'/'.$address->pluck('state')[0] : '';
+                                    $address = Address::where("id", $get("address_id"));
+                                    $address_name = $address && count($address->pluck("name")) > 0 ? $address->pluck("name")[0] : "";
+                                    $address_street = $address && count($address->pluck("address")) > 0 && count($address->pluck("street_number")) > 0 && count($address->pluck("city")) > 0 ? $address->pluck("address")[0]."-".$address->pluck("street_number")[0].", ".$address->pluck("city")[0] : "";
+                                    $address_state = $address && count($address->pluck("province")) > 0 && count($address->pluck("state")) > 0 ? $address->pluck("province")[0]."/".$address->pluck("state")[0] : "";
 
                                     $supplier = Supplier::where('id', $get('supplier_id'));
-                                    $supplier_name = $supplier ? $supplier->pluck('name')[0] : '';
+                                    $supplier_name = $supplier && count($supplier->pluck('name')) > 0 ? $supplier->pluck('name')[0] : '';
 
                                     $orderDetail = OrderDetail::where("order_id", $get("id"));
                                     $totalPrice = 0;
