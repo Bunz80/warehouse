06bd8d0bc7c8c6cce2f245dc9c4cb0ce15c86a70
Bunz80 <sp.bunz@gmail.com>
2022-12-09 18:51:45 +0100

    Style OrderPrint

--- app/Http/Controllers/OrderPrintController.php
+++ app/Http/Controllers/OrderPrintController.php
@@ -58,6 +58,12 @@ class OrderPrintController extends Controller
 
         //Initialize variables
         $style = $header = $destination = $footer_company = $output = '';
+        $style = '  <!-- Latest compiled and minified CSS -->
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+                    <!-- Optional theme -->
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+                    
+                ';
         
         $products = OrderDetail::where('order_id', '=', $id)->get();
 
