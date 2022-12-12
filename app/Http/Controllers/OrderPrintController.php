08934e9488dd8065dc24dd56b13273cbdf428c77
Bunz80 <sp.bunz@gmail.com>
2022-12-12 22:18:21 +0100

    fix pdf

--- app/Http/Controllers/OrderPrintController.php
+++ app/Http/Controllers/OrderPrintController.php
@@ -255,7 +255,7 @@ class OrderPrintController extends Controller
                                 <td>'.$value->code.'</td>
                                 <td><b>'.$value->name.'</b><br/>'.$value->description.'</td>
                                 <td class="text-right td-price">'.$value->quantity.'</td>
-                                <td class="text-right td-price">'.number_format((float) ($value->price_unit), 2).''.$value->currency.'</td>
+                                <td class="text-right td-price">'.number_format((float) ($value->price_unit), 2).' '.$value->currency.'</td>
                                 <td class="text-right td-price">';
 
                 if ($value->discount > 0) {
